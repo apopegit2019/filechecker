519c016f7e3e4787836d25748c811b56059caba5
bukarubonzai <44140980+bukarubonzai@users.noreply.github.com>
2019-03-08 11:26:05 -0500

    Update Missing_Detector.py

--- Missing_Detector.py
+++ Missing_Detector.py
@@ -6,42 +6,42 @@ import sys
 
 def opt_parse():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hf:t:o:', ['--help', '--feed', '--target', '--output'])
-        print(opts)
-        print(args)
+        opts = getopt.getopt(sys.argv[1:], 'hf:t:o:', ['--help', '--feed', '--target', '--output'])
+        for option, argument in opts:
+            if option == '-h':
+                print('-f for feed file location \n -t for target folder \n -o for output location \n --help \n --feed'
+                      ' \n  --target \n --output \n \n')
+            if option in ('-f', '--feed'):
+                feedloc = argument
+            if option in ('-o', '--output'):
+                outloc = argument
+            if option in ('-t', '--target'):
+                targloc = argument
+            if os.path.isdir(targloc):
+                print('The target folder to be searched: %s \n' % targloc)
+            else:
+                print('Target Directory Does Not Exist.')
+                exit()
+            if os.path.isdir(outloc):
+                print('Output will be saved to %s \n' % outloc)
+            else:
+                print('Target Directory Does Not Exist.')
+                exit()
+            if os.path.isfile(feedloc):
+                print('Feed file location is: %s \n' % feedloc)
+            else:
+                print('Target Directory Does Not Exist.')
+                exit()
+            uanswer = input('Are these correct and are you ready to proceed? (Y/N) \n')
+            if uanswer in ('y', 'Y', 'Yes', 'YES'):
+                filecheck(feedloc, outloc, targloc)
+            else:
+                print('Try again')
+                exit()
     except getopt.GetoptError:
         print('-f for feed file location \n -t for target folder \n -o for output location \n --help \n --feed \n '
               '--target \n --output \n \n')
-    if opts == '-h':
-        print('-f for feed file location \n -t for target folder \n -o for output location \n --help \n --feed \n '
-              '--target \n --output \n \n')
-    if opts in ('-f', '--feed'):
-        feedloc = args
-    if opts in ('-o', '--output'):
-        outloc = args
-    if opts in ('-t', '--target'):
-        targloc = args
-    if os.path.isdir(targloc):
-        print('The target folder to be searched: %s \n' % targloc)
-    else:
-        print('Target Directory Does Not Exist.')
-        exit()
-    if os.path.isdir(outloc):
-        print('Output will be saved to %s \n' % outloc)
-    else:
-        print('Target Directory Does Not Exist.')
-        exit()
-    if os.path.isfile(feedloc):
-        print('Feed file location is: %s \n' % feedloc)
-    else:
-        print('Target Directory Does Not Exist.')
-        exit()
-    uanswer = input('Are these correct and are you ready to proceed? (Y/N) \n')
-    if uanswer in ('y', 'Y', 'Yes', 'YES'):
-        filecheck(feedloc, outloc, targloc)
-    else:
-        print('Try again')
-        exit()
+
 
 
 def flength(feedloc):
