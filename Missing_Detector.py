95be261a118c80a0fdc2e4babcfb683de899dc10
bukarubonzai <44140980+bukarubonzai@users.noreply.github.com>
2019-03-08 11:33:07 -0500

    Update Missing_Detector.py

--- Missing_Detector.py
+++ Missing_Detector.py
@@ -3,46 +3,16 @@ import os
 import time
 
 
-def opt_parse():
-    try:
-        opts = getopt.getopt(args, 'hf:t:o:', ['--help', '--feed', '--target', '--output'])
-        print(opts)
-        for option, argument in opts:
-            if option == '-h':
-                print('-f for feed file location \n -t for target folder \n -o for output location \n --help \n --feed'
-                      ' \n  --target \n --output \n \n')
-            if option in ('-f', '--feed'):
-                feedloc = argument
-            if option in ('-o', '--output'):
-                outloc = argument
-            if option in ('-t', '--target'):
-                targloc = argument
-            if os.path.isdir(targloc):
-                print('The target folder to be searched: %s \n' % targloc)
-            else:
-                print('Target Directory Does Not Exist.')
-                exit()
-            if os.path.isdir(outloc):
-                print('Output will be saved to %s \n' % outloc)
-            else:
-                print('Target Directory Does Not Exist.')
-                exit()
-            if os.path.isfile(feedloc):
-                print('Feed file location is: %s \n' % feedloc)
-            else:
-                print('Target Directory Does Not Exist.')
-                exit()
-            uanswer = input('Are these correct and are you ready to proceed? (Y/N) \n')
-            if uanswer in ('y', 'Y', 'Yes', 'YES'):
-                filecheck(feedloc, outloc, targloc)
-            else:
-                print('Try again')
-                exit()
-    except getopt.GetoptError:
-        print('-f for feed file location \n -t for target folder \n -o for output location \n --help \n --feed \n '
-              '--target \n --output \n \n')
-
-
+def filecheck(feedloc, outloc, targloc):
+    lpath = mklog(outloc)
+    feedlen = flength(feedloc)
+    logging(lpath, 'Feed has %d entries. \n' % feedlen)
+    fnd, nfnd = target(feedloc, targloc, feedlen, lpath)
+    logging(lpath, 'Feed Complete')
+    logging(lpath, 'Found %d Files' % fnd)
+    logging(lpath, '%d Files Missing' % nfnd)
+    print('Check Complete \n')
+    exit()
 
 def flength(feedloc):
     with open(feedloc, 'r') as feed:
@@ -59,6 +29,45 @@ def mklog(outloc):
         return
     else:
         return logpath
+
+
+def opt_parse():
+    try:
+        option, argument = getopt.getopt(args, 'hf:t:o:', ['--help', '--feed', '--target', '--output'])
+        print(opts)
+        if option == '-h':
+            print('-f for feed file location \n -t for target folder \n -o for output location \n --help \n --feed'
+                 ' \n  --target \n --output \n \n')
+        if option in ('-f', '--feed'):
+            feedloc = argument
+        if option in ('-o', '--output'):
+            outloc = argument
+        if option in ('-t', '--target'):
+            targloc = argument
+        if os.path.isdir(targloc):
+            print('The target folder to be searched: %s \n' % targloc)
+        else:
+             print('Target Directory Does Not Exist.')
+            exit()
+         if os.path.isdir(outloc):
+             print('Output will be saved to %s \n' % outloc)
+        else:
+            print('Target Directory Does Not Exist.')
+            exit()
+        if os.path.isfile(feedloc):
+            print('Feed file location is: %s \n' % feedloc)
+        else:
+            print('Target Directory Does Not Exist.')
+             exit()
+        uanswer = input('Are these correct and are you ready to proceed? (Y/N) \n')
+        if uanswer in ('y', 'Y', 'Yes', 'YES'):
+             filecheck(feedloc, outloc, targloc)
+        else:
+            print('Try again')
+            exit()
+    except getopt.GetoptError:
+        print('-f for feed file location \n -t for target folder \n -o for output location \n --help \n --feed \n '
+              '--target \n --output \n \n')
 
 
 def logging(logfile, lmsg):
