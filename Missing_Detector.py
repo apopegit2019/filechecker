c4bea6c81ccfb965bd1b47ac7e26392837d1e5fb
bukarubonzai <44140980+bukarubonzai@users.noreply.github.com>
2019-03-08 13:45:03 -0500

    Update Missing_Detector.py

--- Missing_Detector.py
+++ Missing_Detector.py
@@ -98,7 +98,7 @@ def target(feedloc, targloc, feedlen, lpath):
             current += 1
             line = line.rstrip('\n')
             logging(lpath, 'Checking %s/%d: %s' % (feedlen, current, line))
-            file = pathlib.Path(targloc).joinpath(line)
+            file = join(targloc, line)
             if os.path.isfile(file):
                 logging(lpath, '%s exists \n' % line)
                 found += 1
