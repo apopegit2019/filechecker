19acf4209261430da878d156035a32875dda13f1
bukarubonzai <44140980+bukarubonzai@users.noreply.github.com>
2019-03-08 11:57:15 -0500

    Update Missing_Detector.py

--- Missing_Detector.py
+++ Missing_Detector.py
@@ -3,6 +3,7 @@ import os
 import time
 import sys
 
+
 def filecheck(feedloc, outloc, targloc):
     lpath = mklog(outloc)
     feedlen = flength(feedloc)
@@ -13,6 +14,7 @@ def filecheck(feedloc, outloc, targloc):
     logging(lpath, '%d Files Missing' % nfnd)
     print('Check Complete \n')
     exit()
+
 
 def flength(feedloc):
     with open(feedloc, 'r') as feed:
@@ -30,43 +32,55 @@ def mklog(outloc):
     else:
         return logpath
 
+def badinput():
+    print('-f for feed file location \n -t for target folder \n -o for output location \n --help \n --feed \n '
+          '--target \n --output \n \n')
+    exit()
 
 def opt_parse():
     try:
         option, argument = getopt.getopt(sys.argv[1:], 'hf:t:o:', ['--help', '--feed', '--target', '--output'])
         if option == '-h':
-            print('-f for feed file location \n -t for target folder \n -o for output location \n --help \n --feed'
-                 ' \n  --target \n --output \n \n')
-        if option in ('-f', '--feed'):
-            feedloc = argument
-        if option in ('-o', '--output'):
-            outloc = argument
-        if option in ('-t', '--target'):
-            targloc = argument
-        if os.path.isdir(targloc):
-            print('The target folder to be searched: %s \n' % targloc)
+           badinput()
         else:
-            print('Target Directory Does Not Exist.')
-            exit()
-        if os.path.isdir(outloc):
-            print('Output will be saved to %s \n' % outloc)
-        else:
-            print('Target Directory Does Not Exist.')
-            exit()
-        if os.path.isfile(feedloc):
-            print('Feed file location is: %s \n' % feedloc)
-        else:
-            print('Target Directory Does Not Exist.')
-            exit()
-        uanswer = input('Are these correct and are you ready to proceed? (Y/N) \n')
-        if uanswer in ('y', 'Y', 'Yes', 'YES'):
-             filecheck(feedloc, outloc, targloc)
-        else:
-            print('Try again')
-            exit()
+            if option in ('-f', '--feed'):
+                feedloc = argument
+            else:
+                badinput()
+            if option in ('-o', '--output'):
+                outloc = argument
+            else:
+                badinput()
+            if option in ('-t', '--target'):
+                targloc = argument
+            else:
+                badinput()
+            checklocations(feedloc, targloc, outloc)
     except getopt.GetoptError:
-        print('-f for feed file location \n -t for target folder \n -o for output location \n --help \n --feed \n '
-              '--target \n --output \n \n')
+        badinput()
+
+def checklocations(feedloc, targloc, outloc):
+    if os.path.isdir(targloc):
+        print('The target folder to be searched: %s \n' % targloc)
+    else:
+        print('Target Directory Does Not Exist.')
+        exit()
+    if os.path.isdir(outloc):
+        print('Output will be saved to %s \n' % outloc)
+    else:
+        print('Target Directory Does Not Exist.')
+        exit()
+    if os.path.isfile(feedloc):
+        print('Feed file location is: %s \n' % feedloc)
+    else:
+        print('Target Directory Does Not Exist.')
+        exit()
+    uanswer = input('Are these correct and are you ready to proceed? (Y/N) \n')
+    if uanswer in ('y', 'Y', 'Yes', 'YES'):
+        filecheck(feedloc, outloc, targloc)
+    else:
+        print('Try again')
+        exit()
 
 
 def logging(logfile, lmsg):
